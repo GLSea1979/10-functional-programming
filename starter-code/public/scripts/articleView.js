4ffb98c67a03d60e0b4513f2c61ad490e81d8e37
0blu3 <braelynn.luedtke@gmail.com>
2017-01-16 20:36:07 -0800

    completing todos

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -1,6 +1,6 @@
 'use strict';
-
-// TODO: Wrap the entire contents of this file in an IIFE.
+(function(module)){
+// DONE: Wrap the entire contents of this file in an IIFE.
 // Pass in to the IIFE a module, upon which objects can be attached for later access.
 var articleView = {};
 
@@ -108,8 +108,8 @@ articleView.initIndexPage = function() {
 };
 
 articleView.initAdminPage = function() {
-  // TODO: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
-
+  // DONE: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
+  var template = Handlebars.compile($('#author-template').text());
   // REVIEW: We use `forEach` here because we are relying on the side-effects of the callback function:
   // appending to the DOM.
   // The callback is not required to return anything.
@@ -119,3 +119,5 @@ articleView.initAdminPage = function() {
   $('#blog-stats .articles').text(Article.all.length);
   $('#blog-stats .words').text(Article.numWordsAll());
 };
+module.articleView = articleView;
+}(window));
